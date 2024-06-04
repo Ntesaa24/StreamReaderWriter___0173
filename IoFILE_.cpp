e63a074250698da485e1d26881dff5be96853c4f
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-06-04 10:28:22 +0700

    Add project files.

---
+++ IoFILE_.cpp
@@ -0,0 +1,18 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // untuk menyimpan data berbentuk string
+    string baris;
+
+    // membuka file dalam mode menulis.
+    ofstream outfile;
+    // menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    
